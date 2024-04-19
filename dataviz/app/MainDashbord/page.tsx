1ec80b96896854be4a35d7081c97201492c1e95f
porallapradhyumna <porallap@gmail.com>
2024-04-19 13:18:23 -0500

    changes

--- dataviz/app/MainDashbord/page.tsx
+++ dataviz/app/MainDashbord/page.tsx
@@ -6,6 +6,8 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Masonry from '@mui/lab/Masonry';
 
+import D3Example from '../../components/Plots/example'
+
 const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 80];
 
 
@@ -14,18 +16,19 @@ const heights = [150, 30, 90, 70, 110, 150, 130, 80, 50, 90, 100, 150, 30, 50, 8
 function MainDashbord() {
   return (
     <>
-    {/* <div className="grid grid-rows-3 grid-flow-col gap-4 w-full p-2">
-      <Card  variant="soft" className="row-span-3 h-1/2">01</Card>
-      <Card  variant="soft" className="col-span-2">02</Card>
-      <Card  variant="soft" className="row-span-2 h-1/4 col-span-2 ">03</Card>
-    </div> */}
-    <Masonry columns={4} spacing={2} >
+    <D3Example/>
+
+    {/*<Masonry columns={4} spacing={2} >
     {heights.map((height, index) => (
       <>
-      <Card key={index} variant="soft" sx={{height:height}}>01</Card>
+      <Card key={index} variant="soft" sx={{height:height}}>
+        <Example/>
+        
+      </Card>
+      
       </>
     ))}
-  </Masonry>
+  </Masonry>*/}
  </>
   )
 }
---
+++ dataviz/components/Plots/example.jsx
@@ -0,0 +1,53 @@
+import * as d3 from "d3";
+import {useEffect, useRef, useState} from "react";
+
+import { BarChart } from '@mui/x-charts/BarChart';
+
+
+
+export default function D3Example() {
+    useEffect(() => {
+        d3.csv("https://raw.githubusercontent.com/The-Neuro-Handles/dataviz/main/cleaned_dataset/Cleaned_PLMarch.csv", d3.autoType).then(data => {
+            const columnData = data.map(d => d.Sku);
+            const counts = {};
+            columnData.forEach(value => {
+                counts[value] = (counts[value] || 0) + 1;
+            });
+            console.log(counts);
+        });
+
+      }, []);
+    
+  return (
+    <BarChart
+      xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+      series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+      width={500}
+      height={300}
+    />
+  );
+}
+
+
+
+
+//export async function getServerSideProps() {
+//    // Fetch data from your API or other sources
+//
+//    const data = await d3.csv('https://gist.githubusercontent.com/xrd/9a73ccf7ac1e41b728202f3c70cdb959/raw/9c3a1bde4dee9f0d5f80e705bb4f25353ea6b398/test.csv')
+//    .row(function (d){
+//        console.log( d );
+//        return {
+//            ax: Number(d.ax),
+//            ay: Number(d.ay)
+//        };
+//    })
+//    .get(function (error, data){
+//        console.log(data)
+//    });
+//    return {
+//      props: {
+//        data,
+//      },
+//    };
+//  }
